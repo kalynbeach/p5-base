77b5625384c8e5e603df1d614aa47d1ec28e6ff2
Kalyn Beach <kalynbeach@gmail.com>
2021-08-31 13:29:26 -0700

    Implement basic FFT audio analysis

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -10,6 +10,7 @@
 let src; // Audio input source
 let amp; // Audio amplitude analyzer
 
+let fftAnalysis = true;
 
 //
 // Preload
@@ -27,8 +28,10 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   src = new p5.AudioIn();
   amp = new p5.Amplitude();
+  fft = new p5.FFT();
   src.start();
   amp.setInput(src);
+  fft.setInput(src);
 }
 
 
@@ -37,14 +40,41 @@ function setup() {
 //
 
 function draw() {
-  noStroke();
-  fill(0, 10);
-  rect(0, 0, width, height);
+  if (fftAnalysis) {
+    background(220);
 
-  scale = map(amp.getLevel(), 0, 1.0, 10, width);
-  fill(255);
-  text('click to start', width/2.15, 30);
-  ellipse(width/2, height/2, scale, scale);
+    // Amplitude analysis along the frequency domain
+    let spectrum = fft.analyze();
+    noStroke();
+    fill(255, 0, 255);
+    for (let i = 0; i < spectrum.length; i++) {
+      let x = map(i, 0, spectrum.length, 0, width);
+      let h = -height + map(spectrum[i], 0, 255, height, 0);
+      rect(x, height, width / spectrum.length, h);
+    }
+
+    // Amplitude analysis along the time domain
+    let waveform = fft.waveform();
+    noFill();
+    beginShape();
+    stroke(20);
+    for (let i = 0; i < waveform.length; i++) {
+      let x = map(i, 0, waveform.length, 0, width);
+      let y = map(waveform[i], -1, 1, 0, height);
+      vertex(x, y);
+    }
+    endShape();
+    text('click to start', 20, 20);
+  } else {
+    // Simple ellipse placeholder demo
+    noStroke();
+    fill(0, 10);
+    rect(0, 0, width, height);
+    scale = map(amp.getLevel(), 0, 1.0, 10, width);
+    fill(255);
+    text('click to start', width/2.15, 30);
+    ellipse(width/2, height/2, scale, scale);
+  }
 }
 
 
@@ -53,6 +83,7 @@ function draw() {
 //
 
 function mousePressed() {
+  console.log(`* Mouse clicked!`);
   userStartAudio();
 }
 
